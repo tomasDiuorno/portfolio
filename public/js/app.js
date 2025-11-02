0e1cae0aeab8e5d69df7f88b1c0437701eac1f02
Tomas Diuorno <105467737+tomasDiuorno@users.noreply.github.com>
2025-11-02 20:58:44 -0300

    Diseño banner terminado, comienzo con about-me

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,6 @@ const text = "</ tomiDev >"
 const title = document.getElementById("name-logo");
 
 let i = 0;
-
 function type(){
     if(i < text.length) {
         title.textContent += text.charAt(i);
@@ -10,5 +9,4 @@ function type(){
         setTimeout(type, 100);
     }
 }
-
 type();
